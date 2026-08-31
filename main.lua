2dc6bf38937d4b6327e4dc7308e6d206cac390b7
sqlongithub <nniclasss95@gmail.com>
2026-08-31 12:47:15 +0200

    scores

--- main.lua
+++ main.lua
@@ -1,14 +1,27 @@
 --[[pod_format="raw",created="2026-08-31 10:12:09",modified="2026-08-31 10:12:09",revision=0]]
 
 function _init()
+	scoresub_init("messenger_room-1")
+
+	
 	scr = { w = 160, h = 168 }
 	window(scr.w, scr.h, {resizable = true})
 	
 	gui = create_gui({x = 0, y = 0, width = scr.w, height = scr.h})
+
+	text_input = gui:attach_text_editor {
+		x = 5, y = 50, width = 150, height = 15,
+		key_callback = {
+			enter=function(self)
+				scoresub(0, {text = self:get_text()[1]})
+			end
+		}
+	}
 end
 
 function _draw() 
-	cls(1)
+	cls()
+
 	gui:draw_all() 
 end
 
